59e906526303de979e0e0b7842d7a30e0d92518b
Nikita Prokopov <prokopov@gmail.com>
2015-02-09 04:27:03 +0600

    or, or-join, not and not-join parsers

--- src/datascript/where_parser.cljs
+++ src/datascript/where_parser.cljs
@@ -1,5 +1,6 @@
 (ns datascript.where-parser
   (:require
+    [clojure.set :as set]
     [datascript.parser :as dp])
   (:require-macros
     [datascript :refer [raise]]))
@@ -24,13 +25,14 @@
 ;; or-join-clause    = [ src-var? 'or-join' rule-vars (clause | and-clause)+ ]
 ;; and-clause        = [ 'and' clause+ ]
 
+(declare parse-clause)
 
 (defrecord Pattern   [source pattern])
 (defrecord Predicate [fn args])
 (defrecord Function  [fn args binding])
-(defrecord RuleExpr  [source name args])         ;; TODO rule with constant as argument
-(defrecord Not       [source join-vars clauses]) ;; TODO support for default source
-(defrecord Or        [source required-join-vars join-vars clauses])
+(defrecord RuleExpr  [source name args]) ;; TODO rule with constant or '_' as argument
+(defrecord Not       [source vars clauses])
+(defrecord Or        [source rule-vars clauses])
 (defrecord And       [clauses])
 
 (defn parse-pattern-el [form]
@@ -91,14 +93,104 @@
             (RuleExpr. source* name* args*)
           )))))
 
+(defn- collect-vars
+  ([form]
+    (->> (collect-vars [] form)
+         distinct vec))
+  ([acc form]
+    (cond
+      (instance? Not form)         (into acc (.-vars form))
+      (instance? Or form)          (collect-vars acc (.-rule-vars form))
+      (instance? dp/Variable form) (conj acc form)
+      (seqable? form)              (reduce collect-vars acc form)
+      :else acc)))
+
+(defn- validate-join-vars [vars clauses form]
+  (let [undeclared (set/difference (set vars) (set (collect-vars clauses)))]
+    (when-not (empty? undeclared)
+      (raise "Join variable not declared inside clauses: " (mapv :symbol undeclared)
+             {:error :parser/where, :form form})))
+  (when (empty? vars)
+    (raise "Join variables should not be empty"
+           {:error :parser/where, :form form})))
+
+(defn- validate-not [clause form]
+  (validate-join-vars (.-vars clause) (.-clauses clause) form)
+  clause)
+
+(defn parse-not [form]
+  (when-let [[source* next-form] (take-source form)]
+    (let [[sym & clauses] next-form]
+      (when (= 'not sym)
+        (if-let [clauses* (dp/parse-seq parse-clause clauses)]
+          (-> (Not. source* (collect-vars clauses*) clauses*)
+              (validate-not form))
+          (raise "Cannot parse 'not' clause, expected [ src-var? 'not' clause+ ]"
+                 {:error :parser/where, :form form}))))))
+
+(defn parse-not-join [form]
+  (when-let [[source* next-form] (take-source form)]
+    (let [[sym vars & clauses] next-form]
+      (when (= 'not-join sym)
+        (let [vars*    (dp/parse-seq dp/parse-variable vars)
+              clauses* (dp/parse-seq parse-clause clauses)]
+          (if (and vars* clauses*)
+            (-> (Not. source* vars* clauses*)
+                (validate-not form))
+            (raise "Cannot parse 'not-join' clause, expected [ src-var? 'not-join' [variable+] clause+ ]"
+                   {:error :parser/where, :form form})))))))
+
+(defn validate-or [clause form]
+  (let [{{required :required
+          free     :free} :rule-vars
+         clauses          :clauses} clause
+        vars (concat required free)]
+    (doseq [clause clauses]
+      (validate-join-vars vars [clause] form))
+    clause))
+
+(defn parse-and [form]
+  (when (and (sequential? form)
+             (= 'and (first form)))
+    (let [clauses* (dp/parse-seq parse-clause (next form))]
+      (if (not-empty clauses*)
+        (And. clauses*)
+        (raise "Cannot parse 'and' clause, expected [ 'and' clause+ ]"
+               {:error :parser/where, :form form})))))
+
+(defn parse-or [form]
+  (when-let [[source* next-form] (take-source form)]
+    (let [[sym & clauses] next-form]
+      (when (= 'or sym)
+        (if-let [clauses* (dp/parse-seq (some-fn parse-and parse-clause) clauses)]
+          (-> (Or. source* (dp/RuleVars. nil (collect-vars clauses*)) clauses*)
+              (validate-or form))
+          (raise "Cannot parse 'or' clause, expected [ src-var? 'or' clause+ ]"
+                 {:error :parser/where, :form form}))))))
+
+(defn parse-or-join [form]
+  (when-let [[source* next-form] (take-source form)]
+    (let [[sym vars & clauses] next-form]
+      (when (= 'or-join sym)
+        (let [vars*    (dp/parse-rule-vars vars)
+              clauses* (dp/parse-seq parse-clause clauses)]
+          (if (and vars* clauses*)
+            (-> (Or. source* vars* clauses*)
+                (validate-or form))
+            (raise "Cannot parse 'or-join' clause, expected [ src-var? 'or-join' [variable+] clause+ ]"
+                   {:error :parser/where, :form form})))))))
+
+
 (defn parse-clause [form]
   (or 
-;;       (parse-not form)
-;;       (parse-or form)
-      (parse-pred form)
-      (parse-fn form)
+      (parse-not       form)
+      (parse-not-join  form)
+      (parse-or        form)
+      (parse-or-join   form)
+      (parse-pred      form)
+      (parse-fn        form)
       (parse-rule-expr form)
-      (parse-pattern form)
+      (parse-pattern   form)
       (raise "Cannot parse clause, expected (data-pattern | pred-expr | fn-expr | rule-expr | not-clause | not-join-clause | or-clause | or-join-clause)"
              {:error :parser/where, :form form} )))
 
--- test/datascript/test/where_parser.cljs
+++ test/datascript/test/where_parser.cljs
@@ -25,41 +25,39 @@
 )
 
 (deftest pred
-  (are [clause pattern] (= (dwp/parse-clause clause) pattern)
-    '[[pred ?a 1]]
+  (are [clause res] (= (dwp/parse-clause clause) res)
+    '[(pred ?a 1)]
     (dwp/Predicate. (dp/PlainSymbol. 'pred) [(dp/Variable. '?a) (dp/Constant. 1)])
        
-    '[[pred]]
+    '[(pred)]
     (dwp/Predicate. (dp/PlainSymbol. 'pred) [])
   
-    '[[?custom-pred ?a]]
+    '[(?custom-pred ?a)]
     (dwp/Predicate. (dp/Variable. '?custom-pred) [(dp/Variable. '?a)])
 ))
 
 (deftest fn
-  (are [clause pattern] (= (dwp/parse-clause clause) pattern)
-    '[[fn ?a 1] ?x]
+  (are [clause res] (= (dwp/parse-clause clause) res)
+    '[(fn ?a 1) ?x]
     (dwp/Function. (dp/PlainSymbol. 'fn) [(dp/Variable. '?a) (dp/Constant. 1)] (dp/BindScalar. (dp/Variable. '?x)))
        
-    '[[fn] ?x]
+    '[(fn) ?x]
     (dwp/Function. (dp/PlainSymbol. 'fn) [] (dp/BindScalar. (dp/Variable. '?x)))
        
-    '[[?custom-fn] ?x]
+    '[(?custom-fn) ?x]
     (dwp/Function. (dp/Variable. '?custom-fn) [] (dp/BindScalar. (dp/Variable. '?x)))
 
-    '[[?custom-fn ?arg] ?x]
+    '[(?custom-fn ?arg) ?x]
     (dwp/Function. (dp/Variable. '?custom-fn) [(dp/Variable. '?arg)] (dp/BindScalar. (dp/Variable. '?x)))))
 
 (deftest rule-expr
-  (are [clause pattern] (= (dwp/parse-clause clause) pattern)
+  (are [clause res] (= (dwp/parse-clause clause) res)
     '(friends ?x ?y)
-    (dwp/RuleExpr. (dp/DefaultSrc.) (dp/PlainSymbol. 'friends) [(dp/Variable. '?x) (dp/Variable. '?y)]))
+    (dwp/RuleExpr. (dp/DefaultSrc.) (dp/PlainSymbol. 'friends) [(dp/Variable. '?x) (dp/Variable. '?y)])
   
-  (are [clause pattern] (= (dwp/parse-clause clause) pattern)
     '(friends "Ivan" _)
-    (dwp/RuleExpr. (dp/DefaultSrc.) (dp/PlainSymbol. 'friends) [(dp/Constant. "Ivan") (dp/Placeholder.)]))
+    (dwp/RuleExpr. (dp/DefaultSrc.) (dp/PlainSymbol. 'friends) [(dp/Constant. "Ivan") (dp/Placeholder.)])
 
-  (are [clause pattern] (= (dwp/parse-clause clause) pattern)
     '($1 friends ?x ?y)
     (dwp/RuleExpr. (dp/SrcVar. '$1) (dp/PlainSymbol. 'friends) [(dp/Variable. '?x) (dp/Variable. '?y)]))
 
@@ -68,3 +66,178 @@
   
   (is (thrown-with-msg? ExceptionInfo #"Cannot parse rule-expr arguments"
         (dwp/parse-clause '(friends something)))))
+
+(deftest not-clause
+  (are [clause res] (= (dwp/parse-clause clause) res)
+    '(not [?e :follows ?x])
+    (dwp/Not.
+      (dp/DefaultSrc.)
+      [(dp/Variable. '?e) (dp/Variable. '?x)]
+      [ (dwp/Pattern.
+          (dp/DefaultSrc.)
+          [(dp/Variable. '?e) (dp/Constant. :follows) (dp/Variable. '?x)]) ])
+
+    '(not
+       [?e :follows ?x]
+       [?x _ ?y])
+    (dwp/Not.
+      (dp/DefaultSrc.)
+      [(dp/Variable. '?e) (dp/Variable. '?x) (dp/Variable. '?y)]
+      [ (dwp/Pattern.
+          (dp/DefaultSrc.)
+          [(dp/Variable. '?e) (dp/Constant. :follows) (dp/Variable. '?x)])
+        (dwp/Pattern.
+          (dp/DefaultSrc.)
+          [(dp/Variable. '?x) (dp/Placeholder.) (dp/Variable. '?y)])])
+       
+    '($1 not [?x])
+    (dwp/Not.
+      (dp/SrcVar. '$1)
+      [(dp/Variable. '?x)]
+      [ (dwp/Pattern. (dp/DefaultSrc.) [(dp/Variable. '?x)]) ])
+       
+    '(not-join [?e ?y]
+       [?e :follows ?x]
+       [?x _ ?y])
+    (dwp/Not.
+      (dp/DefaultSrc.)
+      [(dp/Variable. '?e) (dp/Variable. '?y)]
+      [ (dwp/Pattern.
+          (dp/DefaultSrc.)
+          [(dp/Variable. '?e) (dp/Constant. :follows) (dp/Variable. '?x)])
+        (dwp/Pattern.
+          (dp/DefaultSrc.)
+          [(dp/Variable. '?x) (dp/Placeholder.) (dp/Variable. '?y)])])
+       
+    '($1 not-join [?e] [?e :follows ?x])
+    (dwp/Not.
+      (dp/SrcVar. '$1)
+      [(dp/Variable. '?e)]
+      [ (dwp/Pattern.
+          (dp/DefaultSrc.)
+          [(dp/Variable. '?e) (dp/Constant. :follows) (dp/Variable. '?x)]) ])
+  )
+  
+  (is (thrown-with-msg? ExceptionInfo #"Join variable not declared inside clauses: \[\?x\]"
+        (dwp/parse-clause '(not-join [?x] [?y]))))
+  
+  (is (thrown-with-msg? ExceptionInfo #"Join variables should not be empty"
+        (dwp/parse-clause '(not-join [] [?y]))))
+  
+  (is (thrown-with-msg? ExceptionInfo #"Join variables should not be empty"
+        (dwp/parse-clause '(not [_]))))
+  
+  (is (thrown-with-msg? ExceptionInfo #"Cannot parse 'not-join' clause"
+        (dwp/parse-clause '(not-join [?x]))))
+  
+  (is (thrown-with-msg? ExceptionInfo #"Cannot parse 'not' clause"
+      (dwp/parse-clause '(not))))
+  
+  (is (thrown-with-msg? ExceptionInfo #"Join variable not declared inside clauses: \[\?y\]"
+        (dwp/parse-clause '(not-join [?y]
+                             (not-join [?x]
+                               [?x :follows ?y])))))
+)
+
+(deftest or-clause
+  (are [clause res] (= (dwp/parse-clause clause) res)
+    '(or [?e :follows ?x])
+    (dwp/Or.
+      (dp/DefaultSrc.)
+      (dp/RuleVars. nil [(dp/Variable. '?e) (dp/Variable. '?x)])
+      [ (dwp/Pattern.
+          (dp/DefaultSrc.)
+          [(dp/Variable. '?e) (dp/Constant. :follows) (dp/Variable. '?x)]) ])
+
+    '(or
+       [?e :follows ?x]
+       [?e :friend ?x])
+    (dwp/Or.
+      (dp/DefaultSrc.)
+      (dp/RuleVars. nil [(dp/Variable. '?e) (dp/Variable. '?x)])
+      [ (dwp/Pattern.
+          (dp/DefaultSrc.)
+          [(dp/Variable. '?e) (dp/Constant. :follows) (dp/Variable. '?x)])
+        (dwp/Pattern.
+          (dp/DefaultSrc.)
+          [(dp/Variable. '?e) (dp/Constant. :friend) (dp/Variable. '?x)])])
+       
+    '(or
+       [?e :follows ?x]
+       (and
+         [?e :friend ?x]
+         [?x :friend ?e]))
+    (dwp/Or.
+      (dp/DefaultSrc.)
+      (dp/RuleVars. nil [(dp/Variable. '?e) (dp/Variable. '?x)])
+      [ (dwp/Pattern.
+          (dp/DefaultSrc.)
+          [(dp/Variable. '?e) (dp/Constant. :follows) (dp/Variable. '?x)])
+        (dwp/And.
+          [(dwp/Pattern.
+             (dp/DefaultSrc.)
+             [(dp/Variable. '?e) (dp/Constant. :friend) (dp/Variable. '?x)])
+           (dwp/Pattern.
+             (dp/DefaultSrc.)
+             [(dp/Variable. '?x) (dp/Constant. :friend) (dp/Variable. '?e)])]) ])
+       
+    '($1 or [?x])
+    (dwp/Or.
+      (dp/SrcVar. '$1)
+      (dp/RuleVars. nil [(dp/Variable. '?x)])
+      [ (dwp/Pattern. (dp/DefaultSrc.) [(dp/Variable. '?x)]) ])
+       
+    '(or-join [?e]
+       [?e :follows ?x]
+       [?e :friend ?y])
+    (dwp/Or.
+      (dp/DefaultSrc.)
+      (dp/RuleVars. nil [(dp/Variable. '?e)])
+      [ (dwp/Pattern.
+          (dp/DefaultSrc.)
+          [(dp/Variable. '?e) (dp/Constant. :follows) (dp/Variable. '?x)])
+        (dwp/Pattern.
+          (dp/DefaultSrc.)
+          [(dp/Variable. '?e) (dp/Constant. :friend) (dp/Variable. '?y)])])
+       
+    '($1 or-join [[?e] ?x]
+         [?e :follows ?x])
+    (dwp/Or.
+      (dp/SrcVar. '$1)
+      (dp/RuleVars. [(dp/Variable. '?e)] [(dp/Variable. '?x)])
+      [ (dwp/Pattern.
+          (dp/DefaultSrc.)
+          [(dp/Variable. '?e) (dp/Constant. :follows) (dp/Variable. '?x)]) ])
+  )
+  
+  (is (thrown-with-msg? ExceptionInfo #"Join variable not declared inside clauses: \[\?x\]"
+        (dwp/parse-clause '(or-join [?x] [?y]))))
+  
+  (is (thrown-with-msg? ExceptionInfo #"Join variable not declared inside clauses: \[\?y\]"
+        (dwp/parse-clause '(or [?x] [?x ?y]))))
+  
+  (is (thrown-with-msg? ExceptionInfo #"Join variable not declared inside clauses: \[\?y\]"
+        (dwp/parse-clause '(or [?x] [?y]))))
+  
+  (is (thrown-with-msg? ExceptionInfo #"Join variable not declared inside clauses: \[\?x\]"
+        (dwp/parse-clause '(or-join [?x ?y] [?x ?y] [?y]))))
+  
+  (is (thrown-with-msg? ExceptionInfo #"Cannot parse rule-vars"
+        (dwp/parse-clause '(or-join [] [?y]))))
+  
+  (is (thrown-with-msg? ExceptionInfo #"Join variables should not be empty"
+        (dwp/parse-clause '(or [_]))))
+  
+  (is (thrown-with-msg? ExceptionInfo #"Cannot parse 'or-join' clause"
+        (dwp/parse-clause '(or-join [?x]))))
+  
+  (is (thrown-with-msg? ExceptionInfo #"Cannot parse 'or' clause"
+      (dwp/parse-clause '(or))))
+  
+  (is (thrown-with-msg? ExceptionInfo #"Join variable not declared inside clauses: \[\?y\]"
+        (dwp/parse-clause '(or-join [?y]
+                             (or-join [?x]
+                               [?x :follows ?y])))))
+)
+
+  
