2f72892a4876140333627ef4035606dc6e480d1e
Nikita Prokopov <niki@tonsky.me>
2024-04-23 18:48:48 +0200

    Migrated to clj-reload and duti

--- dev/user.clj
+++ dev/user.clj
@@ -1,54 +1,31 @@
 (ns user
   (:require
-    [clojure.core.server :as server]
-    [clojure.java.io :as io]
-    [clojure.test :as t]
-    [clojure.tools.namespace.repl :as ns]))
+    [clj-reload.core :as reload]
+    [duti.core :as duti]))
 
-(ns/set-refresh-dirs "src" "bench" "test" #_"bench_datomic" #_"test_datomic")
+(reload/init
+  {:dirs ["src" "bench" "test" #_"bench_datomic" #_"test_datomic"]
+   :no-reload '#{user}})
 
-(defn reload []
+(defn reload [& [opts]]
   (set! *warn-on-reflection* true)
-  (let [res (ns/refresh)]
-    (if (instance? Throwable res)
-      (do
-        (.printStackTrace ^Throwable res)
-        (throw res))
-      res)))
-
-(def lock
-  (Object.))
-
-(defn position []
-  (let [trace (->> (Thread/currentThread)
-                (.getStackTrace)
-                (seq))
-        el    ^StackTraceElement (nth trace 4)]
-    (str "[" (clojure.lang.Compiler/demunge (.getClassName el)) " " (.getFileName el) ":" (.getLineNumber el) "]")))
-
-(defn p [form]
-  `(let [t# (System/currentTimeMillis)
-         res# ~form]
-     (locking lock
-       (println (str "#p" (position) " " '~form " => (" (- (System/currentTimeMillis) t#) " ms) " res#)))
-     res#))
+  (let [res (reload/reload opts)
+        cnt (count (:loaded res))]
+    (str "Reloaded " cnt " namespace" (when (not= 1 cnt) "s"))))
 
 (defn -main [& args]
-  ;; setup repl
   (let [args (apply array-map args)
-        port (or
-               (some-> (get args "--port") parse-long)
-               (+ 1024 (rand-int 64512)))
-        file (io/file ".repl-port")]
-    (println "Started Server Socket REPL on port" port)
-    (spit file port)
-    (.deleteOnExit file)
-    (server/start-server
-      {:name          "repl"
-       :port          port
-       :accept        'clojure.core.server/repl
-       :server-daemon false})))
+        ;; starting app
+        _    (set! *warn-on-reflection* true)
+        _    (require 'datascript.test)
+        ;; starting socket repl
+        port (some-> (get args "--port") parse-long)
+        _    (duti/start-socket-repl {:port port})]))
 
 (defn test-all []
   (reload)
-  (t/run-all-tests #"datascript\..*"))
+  (duti/test #"datascript\.test\..*"))
+
+(defn -test-main [_]
+  (reload {:only #"datascript\.test\..*"})
+  (duti/test-exit #"datascript\.test\..*"))
